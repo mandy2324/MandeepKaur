095fbda5df3b197395b6018e6cd7178e85ce6ef1
unknown <jasdeepsaini30@gmail.com>
2022-01-27 18:15:05 -0500

    adding content edits

--- mandeepkaursaini/src/Components/Footer.js
+++ mandeepkaursaini/src/Components/Footer.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
+import data from "../resumeData.json";
 
 class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if(data){
+      var networks= data.main.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
 
     return (
-      <footer>
+    <>
+     <footer>
 
      <div className="row">
         <div className="twelve columns">
            <ul className="social-links">
               {networks}
            </ul>
-
-           
-
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
      </div>
   </footer>
+  </>
     );
   }
 }
