2a6321039f50a6a0e28008ec5734fa4afaf5d8e5
unknown <jasdeepsaini30@gmail.com>
2022-01-27 18:30:20 -0500

    adding headers image

--- mandeepkaursaini/src/Components/Header.js
+++ mandeepkaursaini/src/Components/Header.js
@@ -1,51 +1,67 @@
 import React, { Component } from 'react';
+import Typical from 'react-typical';
+import data from "../resumeData.json";
 
 class Header extends Component {
-  render() {
+   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      if (data) {
+         var profilepic = "images/" + data.main.image;
+         var networks = data.main.social.map(function (network) {
+            return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+         })
+      }
 
-    return (
-      <header id="home">
+      return (
+         <header id="home">
 
-      <nav id="nav-wrap">
+            <nav id="nav-wrap">
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+               <ul id="nav" className="nav">
+                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                  <li><a className="smoothscroll" href="#about">About</a></li>
+                  <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                  <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
+                  {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
+               </ul>
 
-      </nav>
+            </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">{name}.</h1>
-            <hr />
+            <div className="row banner">
+               <div className="twelve columns">
+                  <img className="profile-pic" src={profilepic} alt="Profile Pic" />
+               </div>
+               <div className="banner-text">
+                  <h1 className="responsive-headline">{data.main.name}</h1>
 
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+                  <Typical
+                     loop={Infinity}
+                     wrapper="h2"
+                     steps={[
+                        'Full Stack Software Engineer', 1000
+                     ]}
+                  />
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
 
-   </header>
-    );
-  }
+
+                  <hr />
+
+                  <ul className="social">
+                     {networks}
+                  </ul>
+               </div>
+            </div>
+
+            <p className="scrolldown">
+               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
+
+         </header>
+      );
+   }
 }
 
 export default Header;
